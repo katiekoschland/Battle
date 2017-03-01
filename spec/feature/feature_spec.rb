b987436d2cee35f84a3c82a9ed9a85efb560c878
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-03-01 12:29:55 +0000

    Added feature test for names

--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -1,8 +1,18 @@
 require 'capybara'
 
-describe "homepage", :type => :feature do
-  it "shows a message" do
+describe "testing feature infrastructure", :type => :feature do
+  it "can run app and shows a message" do
     visit '/'
     expect(page).to have_content "Testing infrastructure working!"
+  end
+
+  context '#names' do
+    it "asks for players' names and saves them" do
+      visit '/'
+      fill_in "name_one", with: "Bruno"
+      fill_in "name_two", with: "Bob"
+      click_button 'Submit'
+      expect(page). to have_content "Bruno vs Bob"
+    end
   end
 end
