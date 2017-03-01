a0d076021fad6ef27252336cf0fe4519fadb9150
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-03-01 14:52:47 +0000

    Improved form

--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -1,14 +1,11 @@
-require 'capybara'
+feature '#names' do
 
-describe "testing feature infrastructure", :type => :feature do
-
-  context '#names' do
-    it "asks for players' names and saves them" do
+    scenario "asks for players' names and saves them" do
       visit '/'
       fill_in "name_one", with: "Bruno"
       fill_in "name_two", with: "Bob"
       click_button 'Submit'
       expect(page).to have_content "Bruno vs Bob"
     end
+    
   end
-end
