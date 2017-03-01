8f80032c3128112097c9c5e02043345be3ba13fb
Katie Koschland <katiekoschland@hotmail.com>
2017-03-01 15:24:04 +0000

    Adding Players Points

--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -1,5 +1,5 @@
-feature '#names' do
 
+feature '#names' do
     scenario "asks for players' names and saves them" do
       visit '/'
       fill_in "name_one", with: "Bruno"
@@ -7,5 +7,14 @@ feature '#names' do
       click_button 'Submit'
       expect(page).to have_content "Bruno vs Bob"
     end
-    
-  end
+end
+
+feature "View hit points" do
+    scenario 'see Player 2 hit points' do
+      visit '/'
+      fill_in "name_one", with: "Bruno"
+      fill_in "name_two", with: "Bob"
+      click_button 'Submit'
+      expect(page).to have_content "Bob: 20HP"
+    end
+end
